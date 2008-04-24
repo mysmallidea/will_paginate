b1641f3c93a4108c9f28c158487057ef770b1a49
Larry Diehl <larry@izea.com>
2008-04-24 09:12:38 -0400

    Added a "paginate" method for ActiveResource. This will paginate the resulting Array if one is returned, otherwise it will also deal properly with servers that return "collection"s.

--- lib/will_paginate/deserializer.rb
+++ lib/will_paginate/deserializer.rb
@@ -9,8 +9,15 @@ module WillPaginate
     end
     
     module ClassMethods
+      def paginate(options = {})
+        options[:params] = {} unless options[:params]
+        options[:params].reverse_merge!(:page => [].paginate.current_page, :per_page => [].paginate.per_page)
+        results = find(:all, options)
+        results.is_a?(WillPaginate::Collection) ? results : results.paginate(:page => options[:params][:page], :per_page => options[:params][:per_page])
+      end
+      
       def instantiate_collection_with_collection(collection, prefix_options = {})
-        if collection["type"] == "collection"
+        if collection.is_a?(Hash) && collection["type"] == "collection"
           collectables = collection.values.find{|c| c.is_a?(Hash) || c.is_a?(Array) }
           collectables = [collectables].compact unless collectables.kind_of?(Array)
           instantiated_collection = WillPaginate::Collection.create(collection["current_page"], collection["per_page"], collection["total_entries"]) do |pager|
--- test/deserializer_test.rb
+++ test/deserializer_test.rb
@@ -2,15 +2,19 @@ require 'helper'
 require 'lib/activeresource_test_case'
 
 class DeserializerTest < Test::Unit::TestCase  
+  # ===================================
+  # = Collection response from server =
+  # ===================================
+  
   def test_collection_without_entries
     collection = create(1, 5, 0)
     projects_xml = collection.to_xml
     
     ActiveResource::HttpMock.respond_to do |mock|      
-      mock.get "/projects.xml", {}, projects_xml
+      mock.get "/projects.xml?page=1&per_page=5", {}, projects_xml
     end
     
-    projects = Client::Project.find(:all) 
+    projects = Client::Project.paginate(:params => {:page => 1, :per_page => 5}) 
     
     assert projects.kind_of?(WillPaginate::Collection)
     
@@ -30,10 +34,10 @@ class DeserializerTest < Test::Unit::TestCase
     projects_xml = collection.to_xml(:root => "projects")
     
     ActiveResource::HttpMock.respond_to do |mock|      
-      mock.get "/projects.xml", {}, projects_xml
+      mock.get "/projects.xml?page=1&per_page=5", {}, projects_xml
     end
     
-    projects = Client::Project.find(:all) 
+    projects = Client::Project.paginate(:params => {:page => 1, :per_page => 5}) 
     
     assert projects.kind_of?(WillPaginate::Collection)
     
@@ -48,15 +52,41 @@ class DeserializerTest < Test::Unit::TestCase
     assert_equal "will_paginate", projects.first.name    
   end
   
+  def test_collection_without_parameters
+    default_page = [].paginate.current_page
+    default_per_page = [].paginate.per_page
+    
+    collection = create(default_page, default_per_page, 1337) {|pager| pager.replace([{:name => "will_paginate" }]) }
+    projects_xml = collection.to_xml(:root => "projects")
+    
+    ActiveResource::HttpMock.respond_to do |mock|      
+      mock.get "/projects.xml?page=#{default_page}&per_page=#{default_per_page}", {}, projects_xml
+    end
+    
+    projects = Client::Project.paginate
+    
+    assert projects.kind_of?(WillPaginate::Collection)
+    
+    assert_equal 1, projects.size
+    
+    assert_equal default_page, projects.current_page
+    
+    assert_equal default_per_page, projects.per_page
+    
+    assert_equal 1337, projects.total_entries    
+    
+    assert_equal "will_paginate", projects.first.name    
+  end
+  
   def test_collection_with_multiple_entries
     collection = create(1, 5, 1337) {|pager| pager.replace([{:name => "will_paginate" }, {:name => "active_resource" }]) }
     projects_xml = collection.to_xml(:root => "projects")
     
     ActiveResource::HttpMock.respond_to do |mock|      
-      mock.get "/projects.xml", {}, projects_xml
+      mock.get "/projects.xml?page=1&per_page=5", {}, projects_xml
     end
     
-    projects = Client::Project.find(:all)
+    projects = Client::Project.paginate(:params => {:page => 1, :per_page => 5}) 
     
     assert projects.kind_of?(WillPaginate::Collection)
     
@@ -72,14 +102,14 @@ class DeserializerTest < Test::Unit::TestCase
   end
   
   def test_collection_on_next_page_within_total
-    collection = create(2, 1, 1337) {|pager| pager.replace([{:name => "will_paginate" }]) }
+    collection = create(2, 1, 2) {|pager| pager.replace([{:name => "will_paginate" }]) }
     projects_xml = collection.to_xml(:root => "projects")
     
     ActiveResource::HttpMock.respond_to do |mock|      
-      mock.get "/projects.xml", {}, projects_xml
+      mock.get "/projects.xml?page=2&per_page=1", {}, projects_xml
     end
     
-    projects = Client::Project.find(:all)
+    projects = Client::Project.paginate(:params => {:page => 2, :per_page => 1}) 
     
     assert projects.kind_of?(WillPaginate::Collection)
     
@@ -89,32 +119,173 @@ class DeserializerTest < Test::Unit::TestCase
     
     assert_equal 1, projects.per_page
     
-    assert_equal 1337, projects.total_entries    
+    assert_equal 2, projects.total_entries    
     
     assert_equal "will_paginate", projects.first.name    
   end
   
   def test_collection_on_next_page_outside_of_total
-    collection = create(2, 2, 1337) {|pager| pager.replace([{:name => "will_paginate" }, {:name => "active_resource" }]) }
+    collection = create(2, 2, 2) {|pager| pager.replace([]) }
     projects_xml = collection.to_xml(:root => "projects")
     
     ActiveResource::HttpMock.respond_to do |mock|      
-      mock.get "/projects.xml", {}, projects_xml
+      mock.get "/projects.xml?page=2&per_page=2", {}, projects_xml
     end
     
-    projects = Client::Project.find(:all)
+    projects = Client::Project.paginate(:params => {:page => 2, :per_page => 2}) 
     
     assert projects.kind_of?(WillPaginate::Collection)
     
-    assert_equal 2, projects.size    
+    assert_equal 0, projects.size    
     
     assert_equal 2, projects.current_page
     
     assert_equal 2, projects.per_page
     
-    assert_equal 1337, projects.total_entries    
-    
+    assert_equal 2, projects.total_entries   
+  end
+  
+  # ==============================
+  # = Array response from server =
+  # ==============================    
+  
+  def test_array_without_entries
+    collection = []
+    projects_xml = collection.to_xml
+  
+    ActiveResource::HttpMock.respond_to do |mock|      
+      mock.get "/projects.xml?page=1&per_page=5", {}, projects_xml
+    end
+  
+    projects = Client::Project.paginate(:params => {:page => 1, :per_page => 5}) 
+  
+    assert projects.kind_of?(WillPaginate::Collection)
+  
+    assert_equal 0, projects.size
+  
+    assert_equal 1, projects.current_page
+  
+    assert_equal 5, projects.per_page
+  
+    assert_equal 0, projects.total_entries    
+  
+    assert_equal nil, projects.first
+  end
+  
+  def test_array_with_one_entry
+    collection = [{:name => "will_paginate" }]
+    projects_xml = collection.to_xml(:root => "projects")
+  
+    ActiveResource::HttpMock.respond_to do |mock|      
+      mock.get "/projects.xml?page=1&per_page=5", {}, projects_xml
+    end
+  
+    projects = Client::Project.paginate(:params => {:page => 1, :per_page => 5}) 
+  
+    assert projects.kind_of?(WillPaginate::Collection)
+  
+    assert_equal 1, projects.size
+  
+    assert_equal 1, projects.current_page
+  
+    assert_equal 5, projects.per_page
+  
+    assert_equal 1, projects.total_entries    
+  
+    assert_equal "will_paginate", projects.first.name    
+  end
+  
+  def test_array_without_parameters
+    default_page = [].paginate.current_page
+    default_per_page = [].paginate.per_page
+  
+    collection = [{:name => "will_paginate" }]
+    projects_xml = collection.to_xml(:root => "projects")
+  
+    ActiveResource::HttpMock.respond_to do |mock|      
+      mock.get "/projects.xml?page=#{default_page}&per_page=#{default_per_page}", {}, projects_xml
+    end
+  
+    projects = Client::Project.paginate
+  
+    assert projects.kind_of?(WillPaginate::Collection)
+  
+    assert_equal collection.paginate.size, projects.size
+  
+    assert_equal default_page, projects.current_page
+  
+    assert_equal default_per_page, projects.per_page
+  
+    assert_equal collection.paginate.total_entries, projects.total_entries    
+  
+    assert_equal "will_paginate", projects.first.name    
+  end
+  
+  def test_array_with_multiple_entries
+    collection = [{:name => "will_paginate" }, {:name => "active_resource" }]
+    projects_xml = collection.to_xml(:root => "projects")
+  
+    ActiveResource::HttpMock.respond_to do |mock|      
+      mock.get "/projects.xml?page=1&per_page=5", {}, projects_xml
+    end
+  
+    projects = Client::Project.paginate(:params => {:page => 1, :per_page => 5}) 
+  
+    assert projects.kind_of?(WillPaginate::Collection)
+  
+    assert_equal 2, projects.size    
+  
+    assert_equal 1, projects.current_page
+  
+    assert_equal 5, projects.per_page
+  
+    assert_equal 2, projects.total_entries    
+  
     assert_equal "active_resource", projects.last.name    
+  end
+  
+  def test_array_on_next_page_within_total
+    collection = [{:name => "will_paginate" }, {:name => "active_resource" }]
+    projects_xml = collection.to_xml(:root => "projects")
+  
+    ActiveResource::HttpMock.respond_to do |mock|      
+      mock.get "/projects.xml?page=2&per_page=1", {}, projects_xml
+    end
+  
+    projects = Client::Project.paginate(:params => {:page => 2, :per_page => 1}) 
+  
+    assert projects.kind_of?(WillPaginate::Collection)
+  
+    assert_equal 1, projects.size    
+  
+    assert_equal 2, projects.current_page
+  
+    assert_equal 1, projects.per_page
+  
+    assert_equal 2, projects.total_entries    
+  
+    assert_equal "active_resource", projects.first.name    
+  end
+  
+  def test_array_on_next_page_outside_of_total
+    collection = [{:name => "will_paginate" }, {:name => "active_resource" }]
+    projects_xml = collection.to_xml(:root => "projects")
+  
+    ActiveResource::HttpMock.respond_to do |mock|      
+      mock.get "/projects.xml?page=2&per_page=2", {}, projects_xml
+    end
+  
+    projects = Client::Project.paginate(:params => {:page => 2, :per_page => 2}) 
+  
+    assert projects.kind_of?(WillPaginate::Collection)
+  
+    assert_equal 0, projects.size    
+  
+    assert_equal 2, projects.current_page
+  
+    assert_equal 2, projects.per_page
+  
+    assert_equal 2, projects.total_entries    
   end
 private
   def create(page = 2, limit = 5, total = nil, &block)
